1d37cbdb5a90186d03e4016e219f928845bccf07
Darren Daly <archerydwd@gmail.com>
2017-02-01 20:34:04 +0000

    working sound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
-import pyttsx
+import pyttsx, time
 
-engine = pyttsx.init()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -10,16 +9,7 @@ team_2_pin = 24
 GPIO.setup(team_1_pin, GPIO.IN)
 GPIO.setup(team_2_pin, GPIO.IN)
 
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate-30)
-volume = engine.getProperty('volume')
-engine.setProperty('volume', volume+0.25)
-engine.setProperty('gender', 'female')
-
-voices = engine.getProperty('voices')
-for voice in voices:
-    engine.setProperty('voice', voice.id)
-    engine.say('The quick brown fox jumped over the lazy dog.')
+engine = pyttsx.init()
 engine.runAndWait()
 
 
@@ -44,6 +34,8 @@ class Game:
     team_2 = Team()
 
     def __init__(self):
+	global engine
+	self.engine = engine
         self.team_1 = Team()
         self.team_2 = Team()
 
@@ -52,53 +44,58 @@ class Game:
         score2 = self.team_2.get_score()
         if score1 == 9 and score2 == 10:
             self.deuce()
-            engine.say('Deuce')
+            self.engine.say('Deuce')
+	    print "Deuce"
         elif score1 == 10 and score2 == 11:
             self.deuce()
-            engine.say('Deuce')
+            self.engine.say('Deuce')
+	    print "Deuce"
         elif score1 == 10 and score2 == 10:
             self.team_1.increment_score()
-            engine.say('Advantage to team 1')
+            self.engine.say('Advantage team 1')
+	    print "Advantage team 1"
         elif score1 == 10 and score2 < 10:
             self.reset()
-            engine.say('Team 1 wins')
+            self.engine.say('Team 1 wins')
+	    print "Team 1 wins"
         elif score1 == 11:
             self.team_1.increment_score()
             self.reset()
-            engine.say('Team 1 wins')
+            self.engine.say('Team 1 wins')
+	    print "Team 1 wins"
         elif score1 < 10:
             self.team_1.increment_score()
-            if self.team_1.get_score() > self.team_2.get_score():
-                engine.say("The score is " + str(self.team_1.get_score()) + ", " + str(self.team_2.get_score()) + " to team 1")
-            else:
-                engine.say("The score is " + str(self.team_2.get_score()) + ", " + str(self.team_1.get_score()) + " to team 2")
-
+	    self.engine.say(str(self.team_1.get_score()) + ", " + str(self.team_2.get_score()))
+	    print str(self.team_1.get_score()) + ", " + str(self.team_2.get_score())
 
     def increment_team_2(self):
         score1 = self.team_2.get_score()
         score2 = self.team_1.get_score()
         if score1 == 9 and score2 == 10:
             self.deuce()
-            engine.say('Deuce')
+            self.engine.say('Deuce')
+	    print "Deuce"
         elif score1 == 10 and score2 == 11:
             self.deuce()
-            engine.say('Deuce')
+            self.engine.say('Deuce')
+	    print "Deuce"
         elif score1 == 10 and score2 == 10:
             self.team_2.increment_score()
-            engine.say('Advantage to team 2')
+            self.engine.say('Advantage team 2')
+	    print "Advantage team 2"
         elif score1 == 10 and score2 < 10:
             self.reset()
-            engine.say('Team 2 wins')
+            self.engine.say('Team 2 wins')
+	    print "Team 2 wins"
         elif score1 == 11:
             self.team_2.increment_score()
             self.reset()
-            engine.say('Team 2 wins')
+            self.engine.say('Team 2 wins')
+	    print "Team 2 wins"
         elif score1 < 10:
             self.team_2.increment_score()
-            if self.team_1.get_score() > self.team_2.get_score():
-                engine.say("The score is " + str(self.team_1.get_score()) + ", " + str(self.team_2.get_score()) + " to team 1")
-            else:
-                engine.say("The score is " + str(self.team_2.get_score()) + ", " + str(self.team_1.get_score()) + " to team 2")
+	    self.engine.say(str(self.team_1.get_score()) + ", " + str(self.team_2.get_score()))
+	    print str(self.team_1.get_score()) + ", " + str(self.team_2.get_score())
 
     def deuce(self):
         self.team_1.set_score(10)
@@ -115,4 +112,5 @@ while True:
         game.increment_team_1()
     elif not GPIO.input(team_2_pin):
         game.increment_team_2()
-    engine.runAndWait()
+    time.sleep(0.5)
+    game.engine.runAndWait()
